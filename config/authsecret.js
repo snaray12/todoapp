b1b0c3447bae193752978b9f9ff9e8f5db1edab6
Sundara N <sundararaman.narayanan@gmail.com>
2017-02-07 22:14:40 +0530

    configs added as runtime param

--- config/authsecret.js
+++ config/authsecret.js
@@ -3,8 +3,8 @@ var port = process.env.PORT || 5000
 module.exports = {
 	'port': port,
     'twitterAuth' : {
-        'clientID'      : '',
-        'clientSecret'  : '',
+        'clientID'      : process.env.clientID,
+        'clientSecret'  : process.env.clientSecret,
         'callbackURL'   : 'http://localhost:'+port+'/auth/tweet/callback'
     }
 };
